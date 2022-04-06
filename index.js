5c637332b81fae583345a3ef12f06a0aab3680ec
Precious <preciousmaregwa1997@gmail.com>
2022-04-06 08:28:44 +0300

    add string

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser ="prince"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+const firstInitial = currentUser[0]
+// const restOfName =currentUser .slice(1)
+const shortGreeting = `Welcome, ${firstInitial} !`;
